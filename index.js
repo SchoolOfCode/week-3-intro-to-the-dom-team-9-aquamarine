0bb035c8946dbb7c69c68e1fa58f671b32058089
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-12-09 14:26:57 +0000

    Completed Ticket 2a

--- index.js
+++ index.js
@@ -37,7 +37,26 @@ const allHighlight = Array.from(document.querySelectorAll(".highlight"));
 
 console.log(allHighlight.map(item =>item.textContent));
 
-//1 f
+// Instructions:
+// Select the first <li> element inside the <ul> with the id of "itemList".
+// Use the parentNode property to access its parent.
+// Log the parent element to the console.
+// Expected Outcome:
+// The console should display the entire <ul> element with the id of "itemList" and all of its <li> children.
+
+// 2a 
+// Using the queryselector, we're accessing the id itemlist and we're taking the first child from the itemlist.
+// Use the parentNode with.... to print the itemlist
+
+const fruitList = document.querySelector("#itemList > li:nth-child(1)").parentNode;
+console.log(fruitList);
 
 
+// Instructions:
+// Select the <ul> element with the id of "itemList".
+// Use the childNodes property to get all its child nodes.
+// Log the child nodes to the console.
+// Expected Outcome:
+// The console should display a collection (NodeList) of child nodes of the <ul> element, which includes the <li> elements as well as any whitespace or line breaks.
 
+// 2b
